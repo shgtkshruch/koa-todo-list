c15ee07b8116d613c634bbc63e25fc0f962065bb
shgtkshruch <shgtk.shruch@gmail.com>
2016-01-29 17:12:47 +0900

    controller

---
+++ controller/account.js
@@ -0,0 +1,32 @@
+var parse = require('co-body');
+var views = require('co-views');
+
+var render = views('views', {default: 'jade'});
+
+module.exports = {
+  get: function *() {
+    this.body = yield render('login', {csrf: this.csrf});
+  },
+
+  login: function *() {
+    var body = yield parse.form(this);
+    try {
+      this.assertCSRF(body);
+    } catch (err) {
+      this.throw(403, 'This CSRF token is invalid.');
+    }
+    if (body.username === process.env.USERNAME && body.password === process.env.PASSWORD) {
+      this.session.authenticated = true;
+      this.status = 303;
+      this.redirect('/');
+    } else {
+      this.throw(400, 'Username and password don\'t match');
+    }
+  },
+
+  logout: function *() {
+    this.session.authenticated = null
+    this.status = 303;
+    this.redirect('/');
+  }
+}
---
+++ controller/todo.js
@@ -0,0 +1,48 @@
+var parse = require('co-body');
+var views = require('co-views');
+var model = require('../model/todo');
+
+var render = views('views', {default: 'jade'});
+
+module.exports = {
+  home: function *() {
+    this.body = yield render('index', {
+      authenticated: this.session.authenticated,
+      todos: yield model.find()
+    });
+  },
+
+  add: function *() {
+    var body = yield parse.form(this);
+    yield model.save(body.title);
+    this.status = 303;
+    this.redirect('/');
+  },
+
+  del: function *() {
+    if (this.session.authenticated) {
+      var body = yield parse(this);
+      yield model.remove(body.id);
+      this.status = 200;
+    } else {
+      this.throw(400);
+    }
+  },
+
+  update: function *() {
+    if (this.session.authenticated) {
+      var todo = yield parse(this);
+
+      if (todo.title) {
+        yield model.update(todo)
+        this.status = 200
+      } else if (todo.completion) {
+        yield model.changeCompletion(todo)
+        this.status = 200
+      }
+
+    } else {
+      this.throw(400);
+    }
+  }
+}
